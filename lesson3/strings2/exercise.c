fe9e3119e9cc9b48742954d866b1cc1d4f50119c
toneskies <tonybadea@frontier.local>
2025-10-27 03:09:36 -0700

    add solution to strings2

--- lesson3/strings2/exercise.c
+++ lesson3/strings2/exercise.c
@@ -7,21 +7,25 @@ void concat_strings(char *str1, const char *str2) {
   while(!found_null) {
     if(str1[i] == '\0') {
       found_null = 1;
+      printf("found null terminator at: %d\n", i);
+      break;
     }
     i++;
   }
+
 
   int j = 0;
   found_null = 0;
   while(!found_null) {
     if(str2[j] == '\0') {
       found_null = 1;
+      break;
     }
-    str1[i] = str2[j];
+    str1[i+j] = str2[j];
     j++; 
   }
   str1[i+j] = '\0';
 
-  printf("%s", str1);
+  printf("%s\n", str1);
 
 }
--- lesson3/strings2/main.c
+++ lesson3/strings2/main.c
@@ -3,7 +3,7 @@
 
 int main() {
     printf("Entering main.\n");
-    char *str1 = "Hey";
+    char str1[10] = "Hey";
     char *str2 = " Man.";
 
     concat_strings(str1, str2);
